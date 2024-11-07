1bda7b7030e9c5a696ed8effe18a6ee53c937aa6
박건우 <woorl201362@gmail.com>
2024-11-07 09:17:04 +0900

    js적용

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// 페이지 아래로 스크롤 시 header 스타일링 적용
+const header = document.querySelector('.header')
+
+// 요소의 총 높이
+const headerHeight = header.offsetHeight
+
+// console.log(headerHeight)
+document.addEventListener('scroll', () => {
+    if(window.scrollY > headerHeight){
+        console.log(scrollY)
+        header.classList.add('header--dark')
+    }else{
+        header.classList.remove('header--dark')
+    }
+})
+
+// Home 섹션을 아래로 스크롤시 투명하게 함
+const home = document.querySelector('.home__container')
+const homeHeight = home.offsetHeight
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight
+})
+
+
+// Arrow up 버튼을 아래로 스크롤 시 투명하게 처리함
+const arrowUp = document.querySelector('.arrow-up')
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.style.opacity = 1
+    }else{
+        arrowUp.style.opacity = 0
+    }
+})
+
+// Navbar 토글버튼 클릭 처리
+const NavbarMenu = document.querySelector('.header__menu')
+const NavbarToggle = document.querySelector('.header__toggle')
+NavbarToggle.addEventListener('click', () => {
+    NavbarMenu.classList.toggle('open')
+})
+
+// Navbar 메뉴 클릭 시 메뉴를 자동으로 닫아줌
+NavbarMenu.addEventListener('click', () => {
+    NavbarMenu.classList.remove('open')
+})
